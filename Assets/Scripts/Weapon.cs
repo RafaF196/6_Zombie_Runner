4ac1a9d0046167afdbbc30ed4b438cd5d3451810
RafaF196 <RafaF196@gmail.com>
2020-12-12 15:33:04 +0100

    Video 9

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -7,6 +7,8 @@ public class Weapon : MonoBehaviour
 {
     [SerializeField] Camera FPCamera;
     [SerializeField] float range = 100f;
+    [SerializeField] float weaponDamage = 2f;
+    [SerializeField] ParticleSystem muzzleFlash;
 
     // Update is called once per frame
     void Update()
@@ -19,8 +21,27 @@ public class Weapon : MonoBehaviour
 
     private void Shoot()
     {
+        PlayMuzzleFlash();
+        ProcessRayCast();
+    }
+
+    private void PlayMuzzleFlash()
+    {
+        muzzleFlash.Play();
+    }
+
+    private void ProcessRayCast()
+    {
         RaycastHit hit;
-        Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range);
-        //hit.transform.name
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        {
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (target == null) return;
+            target.TakeDamage(weaponDamage);
+        }
+        else
+        {
+            return;
+        }
     }
 }
