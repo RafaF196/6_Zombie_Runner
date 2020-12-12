ea0effc9e07b6f68dad4a79bb7c052f5301764fb
RafaF196 <RafaF196@gmail.com>
2020-12-12 18:00:42 +0100

    Video 22

---
+++ Assets/Scripts/WeaponZoom.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponZoom : MonoBehaviour
+{
+    [SerializeField] Camera mainCamera;
+    [SerializeField] float zoomedOutFOV = 60;
+    [SerializeField] float zoomedInFOV = 20;
+
+    bool zoomedInToggle = false;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (!zoomedInToggle)
+            {
+                zoomedInToggle = true;
+                mainCamera.fieldOfView = zoomedInFOV;
+            }
+            else
+            {
+                zoomedInToggle = false;
+                mainCamera.fieldOfView = zoomedOutFOV;
+            }
+        }
+    }
+}
