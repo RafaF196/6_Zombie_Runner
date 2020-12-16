c695857f50eb287058597a583b168cf4a48be5ef
RafaF196 <RafaF196@gmail.com>
2020-12-16 17:19:15 +0100

    Last commit

---
+++ Assets/Scripts/DisplayDamage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayDamage : MonoBehaviour
+{
+    [SerializeField] Canvas damageCanvas;
+    [SerializeField] float damageTime = 0.3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        damageCanvas.enabled = false;
+    }
+
+    public void ShowDamageCanvas()
+    {
+        StartCoroutine(ShowSplatter());
+    }
+
+    IEnumerator ShowSplatter()
+    {
+        damageCanvas.enabled = true;
+        yield return new WaitForSeconds(damageTime);
+        damageCanvas.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
